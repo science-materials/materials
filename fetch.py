5b0f1f985ba40c82e90d79079a57c1083d990991
science-materials <nanonauka@gmail.com>
2026-07-27 20:27:49 +0700

    Update fetch.py

--- fetch.py
+++ fetch.py
@@ -84,4 +84,4 @@ if __name__ == "__main__":
     papers = fetch_papers()
     save_articles(papers)
     
-print(f"=== DEBUG: Scripta finished. Saved {len(reslts)} articles ===")
+print(f"=== DEBUG: Scripta finished. Saved {len(results)} articles ===")
